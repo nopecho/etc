6b2224753596712f1866bd223f163cd568dd2002
nopecho <92011565+nopecho@users.noreply.github.com>
2021-10-16 18:44:04 +0900

    Update AccountingIFApp.java
    배열

--- studing_java/MyApp/src/AccountingIFApp.java
+++ studing_java/MyApp/src/AccountingIFApp.java
@@ -3,13 +3,13 @@ import javax.swing.JOptionPane;
 public class AccountingIFApp {
 
 	public static void main(String[] args) {
-//		ValueOfSupply: °ø±Ş°¡
-//		ExpenseRate: ¸ÅÀÔ°¡ ºñÀ²
-//		VatRate: ºÎ°¡°¡Ä¡¼¼ ºñÀ²
-//		VAT: ºÎ°¡°¡Ä¡¼¼
-//		Total: ÃÑ °¡°İ
-//		Expense: ¸¶Áø
-//		Income: ¼øÀÌÀÍ
+//		ValueOfSupply: ê³µê¸‰ê°€
+//		ExpenseRate: ë§¤ì…ê°€ ë¹„ìœ¨
+//		VatRate: ë¶€ê°€ê°€ì¹˜ì„¸ ë¹„ìœ¨
+//		VAT: ë¶€ê°€ê°€ì¹˜ì„¸
+//		Total: ì´ ê°€ê²©
+//		Expense: ë§ˆì§„
+//		Income: ìˆœì´ìµ
 		
 		String Value = JOptionPane.showInputDialog("Enter a WON");
 		double ValueOfSupply = Double.parseDouble(Value);
@@ -23,16 +23,21 @@ public class AccountingIFApp {
 		double Dividend1;
 		double Dividend2;
 		double Dividend3;
+		double[] DividendRates = new double[4];// double[] <- doubleí˜• ë°ì´í„°ë¡œ ì´ë£¨ì–´ì§„ ë°°ì—´
+		DividendRates[0] = 0.5;
+		DividendRates[1] = 0.3; 
+		DividendRates[2] = 0.1;
+		DividendRates[3] = 1.0;
 		
-		if(Income > 10000.0) {
-		Dividend1 = Income *0.5;
-		Dividend2 = Income *0.3;
-		Dividend3 = Income *0.2;
+		if(Income > 10000.0) { //ë§Œì•½ ìˆœì´ìµì´ 10000ì›ë³´ë‹¤ ë§ìœ¼ë©´
+		Dividend1 = Income *DividendRates[0];
+		Dividend2 = Income *DividendRates[1];
+		Dividend3 = Income *DividendRates[2];
 		}
-		else {
-		Dividend1 = Income *1.0;
-		Dividend2 = Income *0.0;
-		Dividend3 = Income *0.0;			
+		else { //ê·¸ê²Œ ì•„ë‹ˆë¼ë©´
+		Dividend1 = Income *DividendRates[3];
+		Dividend2 = Income *0;
+		Dividend3 = Income *0;			
 		}
 		
 		System.out.println("Value of supply : "+ValueOfSupply);		
