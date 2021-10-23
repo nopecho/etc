1c8c1a8b8541ba993b6fabdf9f227bbcf876b27d
nopecho <92011565+nopecho@users.noreply.github.com>
2021-10-23 14:06:19 +0900

    Add files via upload

---
+++ studing_java/java_flow_control/loopApp.java
@@ -0,0 +1,23 @@
+package java_flow_control;
+
+public class loopApp {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println("====while====");
+		int i =0;
+		while(i<3) { 
+			System.out.println(23);
+			System.out.println(23);
+			i++;
+			
+		}
+		System.out.println("====for====");
+		for (int j=0;j<3;j++) {
+			System.out.println(23);
+			System.out.println(23);
+		}
+
+	}
+
+}
